75a29f54d13ad86058a2e32ae049a6c29d67aae0
komiyamma <komiyama0629@gmail.com>
2023-03-09 06:36:54 +0900

    C# での ChatGPT の 非同期アウトプット

---
+++ OpenAI_ChatGpt3.5turbo_Async/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using OpenAI.GPT3;
+using OpenAI.GPT3.Managers;
+using OpenAI.GPT3.ObjectModels;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+
+Console.WriteLine("Hello, World!");
+
+string? key = Environment.GetEnvironmentVariable("OPENAI_KEY");
+if (String.IsNullOrEmpty(key))
+{
+    Console.WriteLine("キーが環境変数に無い");
+    return;
+}
+
+var openAiService = new OpenAIService(new OpenAiOptions()
+{
+    ApiKey = key
+});
+
+var completionResult = openAiService.ChatCompletion.CreateCompletionAsStream(new ChatCompletionCreateRequest
+{
+    Messages = new List<ChatMessage>
+    {
+        ChatMessage.FromSystem("You are a helpful assistant."),
+        // ChatMessage.FromUser("pythonの効率的な学び方はなんですか？"),
+        // ChatMessage.FromAssistance("The Los Angeles Dodgers won the World Series in 2020."),
+        ChatMessage.FromUser("C#の効率的な学び方はなんですか？")
+    },
+    Model = Models.ChatGpt3_5Turbo,
+    MaxTokens = 1000 //optional
+});
+await foreach (var completion in completionResult)
+{
+    if (completion.Successful)
+    {
+        Console.Write(completion.Choices.FirstOrDefault()?.Message.Content);
+    }
+    else
+    {
+        if (completion.Error == null)
+        {
+            throw new Exception("Unknown Error");
+        }
+
+        Console.WriteLine($"{completion.Error.Code}: {completion.Error.Message}");
+    }
+}
+Console.WriteLine("Complete");
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# gen_openai_chatgpt35_turbo_sample
-C# - OpenAI - ChatGPT3.5 Turbo の Async処理
+# C# - OpenAI - ChatGPT3.5 Turbo の Async処理
+
+![C# - OpenAI - ChatGPT3.5 Turbo の Async処理r](https://img.shields.io/badge/C&#x266f;_OpenAI_ChatGPT_3.5_Turbo-Async(非同期)-6479ff.svg)
